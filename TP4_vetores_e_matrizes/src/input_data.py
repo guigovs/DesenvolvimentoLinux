5418a01202e1e6877122589c0e463d84d529ec66
Guilherme <guilhermedias2501@gmail.com>
2024-12-02 15:20:45 -0300

    Aperfeiçoamento do tratamento dos dados de entrada

--- TP4_vetores_e_matrizes/src/input_data.py
+++ TP4_vetores_e_matrizes/src/input_data.py
@@ -4,20 +4,24 @@ def input_data():
     matriz_a = []
     for i in range(dimensao):
         linha = input(f'Números da {i+1}ª linha da matriz A: ')
+        linha = linha.strip()
         linha = linha.split(' ')
         while len(linha) != dimensao:
             print(f'É necessário ser informado uma quantidade de {dimensao} números na linha!')
             linha = input(f'Números da {i+1}ª linha da matriz A: ')
+            linha = linha.strip()
             linha = linha.split(' ')
         linha = [int(i) for i in linha]
         matriz_a.append(linha)
 
     print('')
     vetor_b = input(f'Informe os {dimensao} números do vetor B: ')
+    vetor_b = vetor_b.strip()
     vetor_b = vetor_b.split(' ')
     while len(vetor_b) != dimensao:
         print(f'É necessário ser informado uma quantidade de {dimensao} números para o vetor B!')
         vetor_b = input(f'Informe os {dimensao} números do vetor B: ')
+        vetor_b = vetor_b.strip()
         vetor_b = vetor_b.split(' ')
     b = [int(i) for i in vetor_b]
 
